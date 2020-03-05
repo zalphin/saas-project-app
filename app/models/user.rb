1ff4e0d09468f7a3f56e004ef8d811bd3e4537b1
eric <sonic009@hotmail.com>
2020-03-05 21:45:26 +0000

    updated user and application because milia didn't

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+         
+  acts_as_universal_and_determines_account
+  has_one :member, :dependent => :destroy
 end
