38210a1ca39cf356c45e06ce0f25d4a5c8e89b46
eric <sonic009@hotmail.com>
2020-03-05 21:20:45 +0000

    Add email with sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp.sendgrid.net',
+	:port => '587',
+	:authentication => :plain,
+	:user_name => Rails.application.credentials.sendgrid[:username],
+	:password => Rails.application.credentials.sendgrid[:pass],
+	:domain => 'heroku.com',
+	:enable_starttls_auto => true
+}
