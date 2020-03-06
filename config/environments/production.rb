a332fe786745d9b5a3a40bc033c5f17901352460
eric <sonic009@hotmail.com>
2020-03-06 17:30:36 +0000

    fixed various paths for new routing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,5 +107,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'eric-test-saas-project-app.herokuapp.com', :protocol => 'https' }
 end
