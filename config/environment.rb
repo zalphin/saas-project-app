5567bd2836d8d204319901ec755de554387b070a
eric <sonic009@hotmail.com>
2020-03-06 20:14:56 +0000

    testing sendgrid with api key

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-	:address => 'smtp.sendgrid.net',
-	:port => '587',
-	:authentication => :plain,
-	:user_name => Rails.application.credentials.sendgrid[:username],
-	:password => Rails.application.credentials.sendgrid[:pass],
-	:domain => 'heroku.com',
-	:enable_starttls_auto => true
-}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,4 +107,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://eric-test-saas-project-app.herokuapp.com" }
 end
