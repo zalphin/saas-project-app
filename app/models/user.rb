f08d79d9f7ae5dcfe202a2801efd190296cd1d3f
eric <sonic009@hotmail.com>
2020-03-09 20:13:40 +0000

    Added admin bool to db tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
          
   acts_as_universal_and_determines_account
   has_one :member, :dependent => :destroy
+  
+  
+  def is_admin?
+    is_admin
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_224026) do
+ActiveRecord::Schema.define(version: 2020_03_09_201104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_224026) do
     t.bigint "tenant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
